7be88cad3afa181565aab97a0baf96d637bdc34f
Himanshu Jain <himanshu.jain@couchbase.com>
2025-06-20 14:05:25 +0530

    test

--- couchbase_utils/delta_lake_util/delta_lake_util.py
+++ couchbase_utils/delta_lake_util/delta_lake_util.py
@@ -127,6 +127,7 @@ class DeltaLakeUtils:
         extra_packages = [
             "org.apache.hadoop:hadoop-common:3.3.4",
             "com.google.cloud.bigdataoss:gcs-connector:hadoop3-2.2.5",
+            "org.apache.httpcomponents:httpcore:4.4.13",
         ]
 
         builder = SparkSession.builder.appName(app_name).config(conf=conf)
